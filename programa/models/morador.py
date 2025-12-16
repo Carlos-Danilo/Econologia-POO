9cbdec55186ab3f21839ce14c07fa29a6907864b
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-12-16 13:19:10 +0000

    aa

--- programa/models/morador.py
+++ programa/models/morador.py
@@ -1,28 +1,59 @@
-from model.operadores.usuario import Usuario
+import json
+from models.dao import DAO
 
-class Morador(Usuario):
-    def __init__(self, nome, email, fone, senha, pontos = 0):
+
+class Morador:
+    def __init__(self, id, nome, email, fone, senha):
+        self.set_id(id)
         self.set_nome(nome)
         self.set_email(email)
         self.set_fone(fone)
-        self.set_pontos(pontos)
         self.set_senha(senha)
 
-    def __str__(self):
-        return f"{self.__id} - {self.__nome} - {self.__email} - {self.__fone} - {self.__pontos} - {self.__senha}"
-    
-    def set_nome(self, nome):
-        if nome == "":
-            raise ValueError("Nome é obrigatório.")
-        self.__nome = nome
 
-    def set_pontos(self, pontos):
-        if pontos < 0:
-            raise ValueError("Não se pode ter menos de 0 pontos.")
-        self.__pontos = pontos
-        # pergunta ao professor: checagem de tipo é no model ou View?
-    def get_nome(self):
-        return self.__nome
-    
-    def get_pontos(self):
-        return self.__pontos
+    def get_id(self): return self.__id
+    def get_nome(self): return self.__nome
+    def get_email(self): return self.__email
+    def get_fone(self): return self.__fone
+    def get_senha(self): return self.__senha
+
+
+    def set_senha(self, senha):
+        if senha == "": raise ValueError("Senha inválida")
+        self.__senha = senha
+    def set_id(self, id): self.__id = id
+    def set_nome(self, nome):
+        if nome == "": raise ValueError("Nome inválido")
+        self.__nome = nome
+    def set_email(self, email):
+        if email == "": raise ValueError("Email inválido")
+        self.__email = email
+    def set_fone(self, fone):
+        if fone == "": raise ValueError("Telefone inválido")
+        self.__fone = fone
+    def to_json(self):
+        dic = {"id":self.__id, "nome":self.__nome, "email":self.__email,"fone":self.__fone, "senha":self.__senha}
+        return dic
+    @staticmethod
+    def from_json(dic):
+        return Morador(dic["id"], dic["nome"], dic["email"], dic["fone"],dic["senha"])
+    def __str__(self):
+        return f"{self.__id} - {self.__nome} - {self.__email} - {self.__fone}"
+
+class MoradorDAO(DAO):
+    @classmethod
+    def abrir(cls):
+        cls._objetos = []
+        try:
+            with open("Morador.json", mode="r") as arquivo:
+                list_dic = json.load(arquivo)
+                for dic in list_dic:
+                    obj = Morador.from_json(dic)
+                    cls._objetos.append(obj)
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def salvar(cls):
+        with open("Morador.json", mode="w") as arquivo:
+            json.dump(cls._objetos, arquivo, default = Morador.to_json) 
--- programa/teste.py
+++ programa/teste.py
@@ -1,27 +1,27 @@
 from database import Database
 from models.morador import Morador
 from models.coleta import Coleta
-from DAO import MoradorDAO, ColetaDAO
+from dao.morador_dao import MoradorDAO
+from dao.coleta_dao import ColetaDAO
 from datetime import datetime
 
-# Cria o banco e as tabelas (opcional se DAO for em memória)
 db = Database()
 db.criar_tabelas()
 
 # TESTE MORADOR
 morador_dao = MoradorDAO()
-morador = Morador("Carlos", "carlos@email.com", "99999999", 0, "1234")
-morador_dao.inserir(morador)
+m = Morador("Carlos", "carlos@email.com", "99999999", "1234", 0)
+morador_dao.inserir(m)
 
-print("Moradores cadastrados:")
-for m in morador_dao.listar():
-    print(m)
+print("Moradores:")
+for morador in morador_dao.listar():
+    print(morador)
 
 # TESTE COLETA
 coleta_dao = ColetaDAO()
-coleta = Coleta(None, datetime.now().isoformat(), "Coleta de papel", 10, True)
-coleta_dao.inserir(coleta)
+c = Coleta(0, datetime.now().isoformat(), "Coleta de papel", 10, True)
+coleta_dao.inserir(c)
 
-print("\nColetas cadastradas:")
-for c in coleta_dao.listar():
-    print(c)
+print("\nColetas:")
+for coleta in coleta_dao.listar():
+    print(coleta)
